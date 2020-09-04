c49e0f2825ded3955e945f38f217f1abd0a67a96
InnovativeInventor <theinnovativeinventor@gmail.com>
2020-09-04 15:06:32 -0400

    Added user ORM and table features from mongoset

--- bot/database.py
+++ bot/database.py
@@ -1,7 +1,20 @@
 import mongoset
 import env
+from mongoset.model import DocumentModel, Immutable, ModelTable
 
 if not env.DATABASE:
     db = mongoset.connect(db_name="discord-bot")
 else:
     db = mongoset.connect(env.DATABASE, db_name="discord-bot")
+
+class User(DocumentModel):
+    id: int = Immutable()
+    registered: bool = Immutable()
+    registration_step: int
+    full_name: str
+    choate_email: Immutable()
+
+class UserTable(ModelTable[User]):
+    member_class = User
+
+user_table = UserTable(db["user"])
