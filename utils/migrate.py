b83d0793615c3401bad330ea019fd45686fa2700
InnovativeInventor <theinnovativeinventor@gmail.com>
2020-09-06 21:36:40 -0400

    Updated migrate file to set everyone to registered (5)

--- utils/migrate.py
+++ utils/migrate.py
@@ -7,5 +7,5 @@ db_sql = dataset.connect("sqlite:////home/max/test/db.sqlite3")
 print(db_sql.tables)
 for x in db_sql["oauth_record"].all():
     if x and x.get("discord_user_id"):
-        db_user = DBUser(discord_id = x.get("discord_user_id"), registered=True, registration_step=0, choate_email=x.get("school_email"), first_name = x.get("first_name"), last_name = x.get("last_name"), opt_out_email = x.get("opt_out_email"))
+        db_user = DBUser(discord_id = x.get("discord_user_id"), registered=True, registration_step=5, choate_email=x.get("school_email"), first_name = x.get("first_name"), last_name = x.get("last_name"), opt_out_email = x.get("opt_out_email"))
         user_table.create(db_user)
