b3c555faa7f0fb1a1d0a474c91f7b2dd14a7f67f
InnovativeInventor <theinnovativeinventor@gmail.com>
2020-09-07 23:21:12 -0400

    Added better logging for auditing

--- bot/commands.py
+++ bot/commands.py
@@ -59,7 +59,7 @@ async def handle_dm(client, user: discord.User, message: discord.Message):
     if db.check_admin(user):
         for each_command, function in admin_direct_commands:
             if bool(re.fullmatch(each_command, message.content)):
-                info(each_command + " command function executed by " + db.get_db_user(user).choate_email)
+                info(each_command + " command function executed by " + db.get_db_user(user).choate_email + " for " + str(message.content))
                 responses.append(await function(client, user, message))
 
     return responses
